5d777bc174767484fa44c901ab5bca17a5ee6be6
Ethan <ethanlegend7@gmail.com>
2026-01-13 15:59:00 +1100

    build navbar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import NavBar from "./components/features/NavBar";
+
 function App() {
   return (
-    <>
+    <div className="font-inter flex h-dvh w-full">
+      <NavBar title="My Applications" />
       <h1>My App</h1>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/features/NavBar.tsx
@@ -0,0 +1,42 @@
+import useIsMobile from "../../hooks/useIsMobile";
+import menuClose from "../../assets/icons/menu-close.svg";
+import menuOpen from "../../assets/icons/menu-open.svg";
+import { useState, type JSX } from "react";
+
+interface Props {
+  title: string;
+}
+
+const NavBar = ({ title }: Props) => {
+  const [open, setOpen] = useState<boolean>(false);
+  const isMobile: boolean = useIsMobile();
+
+  const navigations = (): JSX.Element => {
+    return (
+      <nav className={`flex gap-2 flex-col`}>
+        <a href="#">My Applications</a>
+        <a href="#">Settings</a>
+      </nav>
+    );
+  };
+
+  if (isMobile)
+    return (
+      <header className="bg-primary">
+        <div className="flex justify-between p-2 items-center ">
+          <h1 className="text-2xl font-bold">{title}</h1>
+          <button onClick={() => setOpen((prev) => !prev)}>
+            <img className="w-12" src={open ? menuClose : menuOpen} />
+          </button>
+        </div>
+        {open && <div className="text-center">{navigations()}</div>}
+      </header>
+    );
+  return (
+    <div className="bg-primary w-40 h-screen sticky top-0 p-2">
+      {navigations()}
+    </div>
+  );
+};
+
+export default NavBar;
